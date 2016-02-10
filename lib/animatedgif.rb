73a9be322d8acdb4dc0dce914205cca3bbbec0d0
Dodi Sabaruddin <ddsbrddn@gmail.com>
2016-02-10 17:38:02 +0700

    First commit

--- lib/animatedgif.rb
+++ lib/animatedgif.rb
@@ -1,4 +1,25 @@
-module Animatedgif
-  
+#require 'animatedgif/version'
+require 'httparty'
 
+module Animatedgif
+  include HTTParty
+  base_uri "animatedgif.me"
+
+  def self.find(id)
+    retrive_url get("/gifs/#{id}.json")
+  end
+
+  def self.tagged(tag)
+    retrive_url get("/#{tag}.json")
+  end
+
+  def self.random(tag)
+    tagged(tag)
+  end
+
+  private
+
+    def retrive_url(response)
+      response.parsed_response["url"]
+    end
 end
